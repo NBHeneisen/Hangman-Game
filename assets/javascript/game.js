7744b0b7730e8f08f902fb604b094632cfb58d24
Nicholas Heneisen <Nicholas Heneisen>
2017-02-12 17:56:19 -0500

    Added basic HTML layout, background images to CSS, image files, a text file, and the beginning of the Hangman game object into JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+var hangman = {
+	"wins": 0
+	"losses": 0
+	"wrongGuesses": 12
+	"words": ["desert", "mesa", "cattle", "cactus", "desperado", "saloon", "cowboy", "coyote", "apache", "comanche", "outlaw", "roadrunner"]
+}
